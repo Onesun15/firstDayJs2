dafa6ed3e50a88cb118edbde42b4f8e9fa67bd00
Onesun15 <sunnysharma15@hotmail.com>
2017-09-25 17:02:36 -0400

    jediName done

---
+++ drills_2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// function jediName(firstName, lastName) {
+//   const jediLastName = lastName.substring(0, 3);
+//   const jediFirstName = firstName.substring(0, 2);
+//   console.log(jediLastName + jediFirstName);
+// }
+
+// jediName('Beyonce', 'Knowles');
