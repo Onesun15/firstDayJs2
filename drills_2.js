283d4615b9bb130e3237bcc7af751e0d3e7f0dc1
Adam Bradbury <f.adambrad@gmail.com>
2017-09-25 15:21:18 -0600

    To infinity final

--- drills_2.js
+++ drills_2.js
@@ -7,3 +7,19 @@
 // }
 
 // jediName('Beyonce', 'Knowles');
+
+//To infinity..
+
+function beyond(num) {
+  if (num === Infinity) {
+    console.log('And beyond'); 
+  } else if (Number.isFinite(num) && num > 0) {
+    console.log('To infinity');
+  } else if   (Number.isFinite(num) && num < 0) {
+    console.log('To negative infinity');
+  } else if (num === 0) {
+    console.log('Staying home');
+  }
+}
+
+beyond(-3);
